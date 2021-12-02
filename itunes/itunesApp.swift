61a617ab2598195d7b69777881474b9521a71e8d
jeffmiralles <sponge2126bob@gmail.com>
2021-12-02 15:22:04 +0800

    initial commit

---
+++ itunes/itunesApp.swift
@@ -0,0 +1,17 @@
+//
+//  itunesApp.swift
+//  itunes
+//
+//  Created by Jefferson Miralles on 12/2/21.
+//
+
+import SwiftUI
+
+@main
+struct itunesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
